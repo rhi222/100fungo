43e5d65c588872e895e6c94b2c5e4215a66430e5
Ryohei Nihiyama <ryouhei222@gmail.com>
2017-01-14 22:39:22 +0900

    その他の用法を記載

--- 1stSec/00_reverseSequence.py
+++ 1stSec/00_reverseSequence.py
@@ -2,5 +2,15 @@
 # -*- coding: utf-8 -*-
 # 00.py
 
-str = "stressed"
+#str = "stressed"
+str = "12345678"
+
 print(str[::-1])
+print(str[::-2])
+# 特定の文字を取得
+print(str[0])
+print(str[-1])
+
+# slice
+print(str[1:3])
+
