3e176dc3e978977d333561e9e5dca19f8bcec552
rhi222 <ryouhei222@gmail.com>
2017-05-02 08:58:27 +0900

    12

---
+++ 2ndSec/12.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# 11.py
+
+import sys
+
+def write_col(source_lines, column_number, filename):
+    col = []
+    for line in source_lines:
+        col.append(line.split()[column_number] + "\n")
+    with open(filename, "w") as writer:
+        writer.writelines(col)
+
+with open(sys.argv[1]) as f:
+    lines = f.readlines()
+
+write_col(lines, 0, "col1.txt")
+write_col(lines, 1, "col2.txt")
